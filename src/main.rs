3ebc173571a894f5e84ce607fae90d75fb15f313
Tim Kendrick <t.kendrick@mwam.com>
2020-12-18 17:44:34 +0000

    First commit

---
+++ src/main.rs
@@ -0,0 +1,5 @@
+// SPDX-FileCopyrightText: 2023 Marshall Wace <opensource@mwam.com>
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileContributor: Tim Kendrick <t.kendrick@mwam.com> https://github.com/timkendrickmw
+fn main() {
+}
