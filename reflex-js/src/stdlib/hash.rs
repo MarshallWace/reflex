c92376a6bf1662ae73a4faf7efbc6549e0f46eaf
Tim Kendrick <t.kendrick@mwam.com>
2022-12-14 11:39:52 +0000

    Change hash() stdlib function from variadic to unary

--- reflex-js/src/stdlib/hash.rs
+++ reflex-js/src/stdlib/hash.rs
@@ -6,16 +6,16 @@ use std::{collections::hash_map::DefaultHasher, hash::Hasher};
 
 use reflex::core::{
     uuid, Applicable, ArgType, Arity, EvaluationCache, Expression, ExpressionFactory,
-    FunctionArity, HeapAllocator, Uid, Uuid,
+    ExpressionListType, FunctionArity, HeapAllocator, ListTermType, RefType, Uid, Uuid,
 };
 
 pub struct Hash {}
 impl Hash {
     pub(crate) const UUID: Uuid = uuid!("8312114e-8e8d-4ab0-a4d6-7a58f4e51f1d");
-    const ARITY: FunctionArity<0, 0> = FunctionArity {
-        required: [],
+    const ARITY: FunctionArity<1, 0> = FunctionArity {
+        required: [ArgType::Strict],
         optional: [],
-        variadic: Some(ArgType::Strict),
+        variadic: None,
     };
     pub fn arity() -> Arity {
         Arity::from(&Self::ARITY)
@@ -40,12 +40,23 @@ impl<T: Expression> Applicable<T> for Hash {
         _allocator: &impl HeapAllocator<T>,
         _cache: &mut impl EvaluationCache<T>,
     ) -> Result<T, String> {
-        let args = args.into_iter();
-        let mut hasher = DefaultHasher::new();
-        for arg in args {
-            std::hash::Hash::hash(&arg.id(), &mut hasher);
+        let mut args = args.into_iter();
+        let parsed_args = args.next().unwrap();
+        match factory.match_list_term(&parsed_args) {
+            Some(parsed_args) => {
+                let mut hasher = DefaultHasher::new();
+                for arg in parsed_args
+                    .items()
+                    .as_deref()
+                    .iter()
+                    .map(|item| item.as_deref())
+                {
+                    std::hash::Hash::hash(&arg.id(), &mut hasher);
+                }
+                let hash = hasher.finish();
+                Ok(factory.create_symbol_term(hash))
+            }
+            _ => Err(format!("Expected list, received {}", parsed_args)),
         }
-        let hash = hasher.finish();
-        Ok(factory.create_symbol_term(hash))
     }
 }
